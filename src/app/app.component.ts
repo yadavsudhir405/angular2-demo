13163324ff3db9b461b1042ca13e773cbae3d42e
yadavsudhir405@gmail.com <sudhiry@thoughtworks.com>
2018-02-17 12:55:26 +0530

    Add First component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,16 @@
-import { Component } from '@angular/core';
+
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:  `
-  <div>
-    <h1>{{pageTitle}}</h1>
-    <div>This is my First Angular App</div>
-  </div>
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <div>This is my first Angular App</div>
+    </div>
   `
-
 })
 
 export class AppComponent {
-  pageTitle = 'Aceme Project Management';
+  pageTitle = 'Acme Project Management';
 }
-
-
